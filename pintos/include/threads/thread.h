00a2dfa88b4678ab1916d26e75fb9a607d44efd0
SISU <atgsisu@gmail.com>
2026-04-25 21:06:25 +0900

    fix: thread_wakeup에서 가짜 head 객체에 접근하던 문제 수정

--- pintos/include/threads/thread.h
+++ pintos/include/threads/thread.h
@@ -145,4 +145,7 @@ int thread_get_load_avg (void);
 
 void do_iret (struct intr_frame *tf);
 
+void thread_sleep (int64_t ticks);
+void thread_wakeup (void);
+
 #endif /* threads/thread.h */
--- pintos/threads/thread.c
+++ pintos/threads/thread.c
@@ -64,7 +64,7 @@ static void do_schedule(int status);
 static void schedule (void);
 static tid_t allocate_tid (void);
 void thread_sleep (int64_t ticks);
-void thread_wakeup ();
+void thread_wakeup (void);
 
 /* Returns true if T appears to point to a valid thread. */
 #define is_thread(t) ((t) != NULL && (t)->magic == THREAD_MAGIC)
@@ -603,27 +603,34 @@ wakeup_less (const struct list_elem *a, const struct list_elem *b, void* aux UNU
 
 void
 thread_sleep (int64_t ticks) {
-	struct thread * cur = thread_current();
 	enum intr_level old_level = intr_disable ();	/* interrupt 방해금지모드 설정 */
+	struct thread * cur = thread_current();
 	int64_t start = timer_ticks ();
+	// printf("now is..... %lld\n", start);
 	cur->wakeup_tick = start + ticks;
+	// printf("===================when wakeup_tick is set.. %lld\n", cur->wakeup_tick);
 	list_insert_ordered(&sleep_list, &(cur->sleep_elem), wakeup_less, NULL);
-	intr_set_level (old_level);						/* interrupt 방해금지모드 해제*/
+	thread_block ();
+	intr_set_level (old_level);						/* interrupt 방해금지모드 해제 */
 }
 
 void 
 thread_wakeup () {
 	enum intr_level old_level = intr_disable ();	/* interrupt 방해금지모드 설정 */
 
-	int64_t cur_tics = timer_ticks();
-	struct thread * sleep_list_head = list_entry (&(sleep_list.head), struct thread, sleep_elem);
+	int64_t cur_tics = timer_ticks ();
+	struct thread * sleep_list_head = list_entry (sleep_list.head.next, struct thread, sleep_elem);
 	struct thread * t = sleep_list_head;
 
-	while (!list_empty (&sleep_list) && t->wakeup_tick <= cur_tics){//큐의 앞에 있는 스레드가 나와야한다면	
+	// printf("DBG: tid: %lld, wakeup: %lld\n", t->tid, t->wakeup_tick);
+
+	while (!list_empty (&sleep_list) && t->wakeup_tick <= cur_tics) {//큐의 앞에 있는 스레드가 나와야한다면	
+		// printf("DBG:::: wakeup start\n");
+
 		struct list_elem *e = list_pop_front (&sleep_list);
 		t = list_entry (e, struct thread, sleep_elem);
-		thread_unblock(t);
+		thread_unblock (t);
 	}	
 	
-	intr_set_level (old_level);						/* interrupt 방해금지모드 해제*/
+	intr_set_level (old_level);						/* interrupt 방해금지모드 해제 */
 }
