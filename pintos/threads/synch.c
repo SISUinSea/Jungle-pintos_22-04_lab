6bba1ac77ac7c478e486ad1bfef96ee3a34dd7ec
이경근 <lsg008@naver.com>
2026-04-28 20:10:38 +0900

    fix: 테스트 중 실수로 주석 풀어놓은 것 다시 주석 처리

--- pintos/threads/synch.c
+++ pintos/threads/synch.c
@@ -73,7 +73,7 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		list_push_back (&sema->waiters, &thread_current ()->elem);
+		// list_push_back (&sema->waiters, &thread_current ()->elem);
 		list_insert_ordered (&(sema->waiters), &thread_current()->elem, priority_more_func, NULL);
 		thread_block ();
 	}
