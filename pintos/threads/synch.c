77984ff1411c38bb96c64aaef6247fac21b23881
이경근 <lsg008@naver.com>
2026-04-28 16:31:19 +0900

    fix: 디버깅용 printf 삭제

--- pintos/threads/synch.c
+++ pintos/threads/synch.c
@@ -309,18 +309,9 @@ cond_wait (struct condition *cond, struct lock *lock) {
 
 	sema_init (&waiter.semaphore, 0);
 	list_push_back (&cond->waiters, &waiter.elem);
-
 	// list_insert_ordered(&cond->waiters, &waiter.elem, priority_more_semaphore_elem_func, NULL); // list_insert_ordered(&리스트, &넣을_구조체->elem, 비교함수, NULL);
-	/* printf("[cond_wait] thread=%s pri=%d cond_waiters=%d\n", // 현재 스레드, 그스레드의 우선순위, 지금까지 몇명 들어왔는지
-       thread_current()->name,
-       thread_current()->priority,
-       (int) list_size(&cond->waiters)); */
 	lock_release (lock);
 	sema_down (&waiter.semaphore);
-	/* printf("[cond_woke] thread=%s pri=%d\n", //
-       thread_current()->name,
-       thread_current()->priority); */
-
 	lock_acquire (lock);
 }
 
@@ -334,10 +325,6 @@ cond_wait (struct condition *cond, struct lock *lock) {
 void
 cond_signal (struct condition *cond, struct lock *lock UNUSED) {
 	struct semaphore_elem waiter;
-	/* printf("[cond_signal] current=%s pri=%d cond_waiters=%d \n", // 현재 스레드, 우선 순위, 몇 명 있는지
-       thread_current()->name,
-       thread_current()->priority,
-       (int) list_size(&cond->waiters)); */
 	ASSERT (cond != NULL);
 	ASSERT (lock != NULL);
 	ASSERT (!intr_context ());
@@ -345,8 +332,6 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED) {
 
 	if (!list_empty (&cond->waiters)){
 		list_sort (&(cond->waiters), priority_more_semaphore_elem_func, NULL); // list_sort(&정렬할_리스트, 비교함수, NULL);
-		
-
 		sema_up (&list_entry (list_pop_front (&cond->waiters),
 					struct semaphore_elem, elem)->semaphore);
 	}
