122fd60f14f8ceb0484392dbafdad7b5a2c0b6a6
이원재 <57494067+Wish-Upon-A-Star@users.noreply.github.com>
2026-04-29 22:01:05 +0900

    fix: 최적화 1

--- pintos/threads/synch.c
+++ pintos/threads/synch.c
@@ -128,15 +128,18 @@ sema_up (struct semaphore *sema) {
 	enum intr_level old_level;
 
 	ASSERT (sema != NULL);
-
+	struct thread* t;
 	old_level = intr_disable ();
 	if (!list_empty (&sema->waiters)){
 		list_sort (&(sema->waiters), priority_more_func, NULL);
-		thread_unblock (list_entry (list_pop_front (&sema->waiters), struct thread, elem));
+		t=list_entry (list_pop_front (&sema->waiters), struct thread, elem);
+		thread_unblock (t);
 	}
 	sema->value++;
 	intr_set_level (old_level);
+	if(t->priority > thread_current()->priority){
 	thread_yield();
+	}
 }
 
 static void sema_test_helper (void *sema_);
