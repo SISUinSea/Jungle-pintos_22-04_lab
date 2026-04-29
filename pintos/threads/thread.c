4bd232ae1969d5303df76d95f6433527d9bc86d3
SISU <atgsisu@gmail.com>
2026-04-29 20:44:07 +0900

    fix: thread 초기화 할 때 base_priority, donators 초기화 안해주는 문제 수정 - 하지만 여전히 테스트는 통과 안 함.

--- pintos/threads/thread.c
+++ pintos/threads/thread.c
@@ -433,7 +433,9 @@ init_thread (struct thread *t, const char *name, int priority) {
 	strlcpy (t->name, name, sizeof t->name);
 	t->tf.rsp = (uint64_t) t + PGSIZE - sizeof (void *);
 	t->priority = priority;
+	t->base_priority = priority;
 	t->magic = THREAD_MAGIC;
+	list_init (&t->donators);
 }
 
 /* Chooses and returns the next thread to be scheduled.  Should
