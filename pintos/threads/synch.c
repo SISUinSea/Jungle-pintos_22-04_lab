7d30bd8ab9673bc936c4a1145de699d7ee6580e7
SISU <atgsisu@gmail.com>
2026-04-29 21:15:42 +0900

    fix: donator_elem을 기준으로 thread 접근해서 값을 계산하도록 하는 별도의 비교 함수 생성 - 여전히 테스트 통과 안 함.

--- pintos/threads/synch.c
+++ pintos/threads/synch.c
@@ -38,6 +38,17 @@ static bool priority_more_func (const struct list_elem* a ,const struct list_ele
 	struct thread* thread_a = list_entry(a,struct thread,elem);
 	struct thread* thread_b = list_entry(b,struct thread,elem);
 
+	// printf("================ a:%d b:%d\n", thread_a->priority, thread_b->priority);
+
+	return thread_a->priority>thread_b->priority;
+}
+
+static bool donate_priority_more_func (const struct list_elem* a ,const struct list_elem* b,void* aux) {
+	struct thread* thread_a = list_entry(a,struct thread, donator_elem);
+	struct thread* thread_b = list_entry(b,struct thread, donator_elem);
+
+	// printf("================ a:%d b:%d\n", thread_a->priority, thread_b->priority);
+
 	return thread_a->priority>thread_b->priority;
 }
 
