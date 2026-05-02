042923a2045ad762eb6ad6654c78bd12e45b6197
이경근 <lsg008@naver.com>
2026-05-02 11:42:43 +0900

    fix: 스택 오버플로우 가능성 제거

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -163,7 +163,8 @@ error:
 int
 process_exec (void *f_name) {
 
-    if ( f_name == NULL) return -1;
+    if ( f_name == NULL)
+		return -1;
     
 	char *cmd_page = f_name;
 	char *arg;
@@ -179,8 +180,8 @@ process_exec (void *f_name) {
 	_if.eflags = FLAG_IF | FLAG_MBS;
 
 	int arg_count=0;
-	char *temp_arg[128]={ 0 };
-	uint64_t addr_arg[128]={ 0 };
+	char *temp_arg[32]={ 0 };
+	uint64_t addr_arg[32]={ 0 };
 	char *next_ptr;
 	int i=0;
 
