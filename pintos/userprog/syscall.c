6d35dbb956aa64e20279608f8f38c433e2416bf9
이경근 <lsg008@naver.com>
2026-05-06 17:15:58 +0900

    feat: SYS_WRITE 구현

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -250,7 +250,13 @@ syscall_handler (struct intr_frame *f) {
 				f->R.rax = size;
 				break;
 			}
-			f->R.rax = -1;
+			struct fd_entry *entry = find_fd_entry (fd);
+			if (entry == NULL) {
+				f->R.rax = -1;
+				break;
+			}
+			
+			f->R.rax = file_write(fd, buf, size);
 			break;
 		}
 		case SYS_CLOSE:
